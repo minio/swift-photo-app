aec84335deaac31dde6ad13fa59fb81ddf5a2d7f
Deepa Mahalingam <Upheus1@Deepas-MacBook-Pro.local>
2016-04-21 18:23:26 -0700

    All the Project & Test files to run the Swift Project

---
+++ SwiftPhotoAppUITests/SwiftPhotoAppUITests.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftPhotoAppUITests.swift
+//  SwiftPhotoAppUITests
+//
+//  Created by Deepa Mahalingam on 4/21/16.
+//  Copyright © 2016 Deepa Mahalingam. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class SwiftPhotoAppUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
