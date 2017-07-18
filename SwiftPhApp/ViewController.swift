e9e9c4c7fc2287420a404cd48e040ded7e04e854
Carter McClellan <carter.s.mcclellan@gmail.com>
2017-07-18 09:17:59 -0700

    Adding all modified files

--- SwiftPhApp/ViewController.swift
+++ SwiftPhApp/ViewController.swift
@@ -1,10 +1,18 @@
 //
-//  ViewController.swift
-//  SwiftPhApp
-//
-//  Created by Carter McClellan on 7/12/17.
-//  Copyright © 2017 Minio. All rights reserved.
-//
+//  SwiftPhotoApp, (C) 2017 Minio, Inc.
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 
 import UIKit
 
@@ -13,7 +21,7 @@ class ViewController: UIViewController {
     @IBAction func refButton(sender: UIButton) {
         
         // Set up the URL Object.
-        let url = URL(string: "http://play.minio.io/PhotoAPIService/minio/photoservice/list")
+        let url = URL(string: "http://play.minio.io:8080/PhotoAPIService-0.0.1-SNAPSHOT/minio/photoservice/list")
         
         // Task fetches the url contents asynchronously.
         let task = URLSession.shared.dataTask(with: url! as URL) {(data, response, error) in
